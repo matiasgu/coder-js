982d62b31653ae7af74cf68f66784bfcd9353a4a
Matias Gutierrez <matias.gutierrez4989@gmail.com>
2022-08-27 20:52:09 -0300

    clase 4

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ for (let i = 0; i < 6; i++) {
 
 }
 alert(`sus 6 productos son : ${lista}`)
-*/
 
 let lista = prompt('ingrese producto')
 let producto = ''
@@ -16,3 +15,26 @@ while (lista != 'esc') {
     lista = prompt('ingrese producto y finalizar con -esc-')
 }
 alert(`su lista de producto es : ${producto}`)
+*/
+
+let montoPagar = prompt('ingrese monto a pagar')
+let formaPago = prompt('ingrese T si para con targeta' + '\n' + 'ingrese E si paga con efectivo')
+
+function descuento(x) {
+    return x * 0.15
+}
+
+function totalPagar(a, b) {
+    return a - b 
+}
+
+if (formaPago === 'T') {
+    console.log(`monto a pagar es: ${montoPagar}`)
+} else if (formaPago === 'E') {
+    console.log('monto a pagar con un 15% de descuento por pago en efectivo es:')
+    console.log(totalPagar(montoPagar, descuento(montoPagar)))
+}
+else {
+    alert('la opcion ingresada es incorrecta')
+}
+
