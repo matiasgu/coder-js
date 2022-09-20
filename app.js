67f35b2d165ab65fa36d0420a6bf5f9cf7fa1ea2
Matias Gutierrez <matias.gutierrez4989@gmail.com>
2022-09-20 14:53:22 -0300

    comit 20-9

--- app.js
+++ app.js
@@ -1,179 +1,47 @@
-
-/*
-let lista = ''
-
-for (let i = 0; i < 6; i++) {
-    lista += prompt('ingrese 6 productos supermercado') + '\n'
-
-}
-alert(`sus 6 productos son : ${lista}`)
-
-let lista = prompt('ingrese producto')
-let producto = ''
-while (lista != 'esc') {
-    producto += lista + '\n'
-    lista = prompt('ingrese producto y finalizar con -esc-')
-}
-alert(`su lista de producto es : ${producto}`)
-
-let montoPagar = prompt('ingrese monto a pagar')
-let formaPago = prompt('ingrese T si para con targeta' + '\n' + 'ingrese E si paga con efectivo')
-
-function descuento(x) {
-    return x * 0.15
-}
-
-function totalPagar(a, b) {
-    return a - b 
-}
-
-if (formaPago === 'T') {
-    console.log(`monto a pagar es: ${montoPagar}`)
-} else if (formaPago === 'E') {
-    console.log('monto a pagar con un 15% de descuento por pago en efectivo es:')
-    console.log(totalPagar(montoPagar, descuento(montoPagar)))
-}
-else {
-    alert('la opcion ingresada es incorrecta')
-}
-
-*/
-
-//ESTO ES CARRITO DE COMPRA CON ARRAY
-/*
-const productos = [
-    {nombre: 'coca cola' , precio: 250},
-    {nombre: 'fanta' , precio: 250},
-    {nombre: 'fernet branca' , precio: 1300},
-    {nombre: 'vino toro' , precio: 200},
-    {nombre: 'viña de balbo' , precio: 350},
-    {nombre: 'manaos naranja' , precio: 190},
-    {nombre: 'vodka' , precio: 800},
-    {nombre: 'ginebra' , precio: 1200},
-    {nombre: 'wisky' , precio: 4000},
-]
-
-let carrito = []
-let seleccion= prompt('desa comprar alguno de los productos')
-
-while (seleccion != 'si' && seleccion != 'no') {
-    alert('por favor ingrese si o no')
-    seleccion = prompt('hola desea comprar algo si o no')
-    
-}
-
-if (seleccion == 'si') {
-    alert('a continuacion nuestra lista de productos')
-    let listaDeProdutos = productos.map(
-        (producto) => producto.nombre + ' ' + producto.precio + '$'
-    )
-    alert (listaDeProdutos.join('\n'))
-} else if (seleccion == 'no'){
-    alert('gracias por venir')
-}
-
-while (seleccion != 'no') {
-    let producto = prompt('agrega un producto a tu carrito')
-    let precio = 0
-
-    if (producto == 'coca cola' || producto == 'fanta' || producto == 'fernet branca' || producto == 'vino toro' || producto == ' viña de balbo' || producto == 'manaos naranja' || producto == 'vodka' || producto == 'ginebra' || producto == 'wisky') {
-        switch (producto) {
-            case 'coca cola':
-                precio = 250
-                break;
-            case 'fanta':
-                precio = 250
-                break;    
-            case 'fernet branca':
-                precio = 1300
-                break;
-            case 'vino toro':
-                precio = 200
-                break;
-            case 'viña de balbo':
-                precio = 350
-                break;
-            case 'manaos naranja':
-                precio = 190
-                break;
-            case 'vodka':
-                precio = 800
-                break;
-            case 'ginebra':
-                precio = 1200
-                break;
-            case 'wisky':
-                precio = 4000
-                break;        
-            default:
-                break;
-        }
-
-        let unidades = parseInt(prompt('cuantas unidades desea llevar'))
-
-        carrito.push({producto, unidades, precio})
-        console.log(carrito)
-    } else {
-        alert('no tenesmos ese producto')
-    }
-
-    seleccion = prompt('desea seguir comprando?')
-
-    while (seleccion === 'no') {
-        alert('gracias por la compra')
-        carrito.forEach((carritoFinal) => {
-            console.log(`producto: ${carritoFinal.producto}, unidades: ${carritoFinal.unidades}, total a pagar por producto ${carritoFinal.unidades * carritoFinal.precio}`)
-        })
-    break
-    }
-}
-*/
 const productos = [
     {
-        nombre: 'coca cola',
-        precio: 250,
-        imagen: './imagen/coca.png'
-    },
-    {
-        nombre: 'fanta',
-        precio: 250,
-        imagen: './imagen/coca.png'
-    },
-    {
-        nombre: 'fernet branca',
-        precio: 1300,
-        imagen: './imagen/coca.png'
-    },
-    {
-        nombre: 'vino toro',
-        precio: 200,
-        imagen: './imagen/coca.png'
-    },
-    {
-        nombre: 'viña de balbo',
-        precio: 350,
-        imagen: './imagen/coca.png'
-    },
-    {
-        nombre: 'manaos naranja',
-        precio: 190,
-        imagen: './imagen/coca.png'
-    },
-    {
-        nombre: 'vodka',
+        id: 1,
+        nombre: 'GRINGA',
+        descripcion:'Pan artesanal, Doble medallon de carne caseros, Queso Cheddar, Panceta, Salsa barbacoa, PAPAS FRITAS',
         precio: 800,
-        imagen: './imagen/coca.png'
+        imagen: './imagen/hambur.png'
     },
     {
-        nombre: 'ginebra',
-        precio: 1200,
-        imagen: './imagen/coca.png'
+        id: 2,
+        nombre: 'REGIONAL',
+        descripcion:'Pan artesanal, Doble medallon de carne caseros, Queso Tybo, Huevo frito, mayonesa, PAPAS FRITAS',
+        precio: 800,
+        imagen: './imagen/hambur.png'
     },
     {
-        nombre: 'wisky',
-        precio: 4000,
-        imagen: './imagen/coca.png'
+        id: 3,
+        nombre: 'CHAMPIGNON',
+        descripcion:'Pan artesanal, Doble medallon de carne caseros, Queso CTybo, Aros de cebolla, Mayonesa, Champignon, PAPAS FRITAS',
+        precio: 850,
+        imagen: './imagen/hambur.png'
     },
+    {
+        id: 4,
+        nombre: 'AZUL',
+        descripcion:'Pan artesanal, Doble medallon de carne caseros, Queso azul, aros de ceebolla caramelizados, Salsa azul, PAPAS FRITAS',
+        precio: 850,
+        imagen: './imagen/hambur.png'
+    },
+    {
+        id: 5,
+        nombre: 'TRIPLE QUESO',
+        descripcion:'Pan artesanal, Doble medallon de carne caseros, Queso Cheddar, Tybo, Azul, Mayonesa, PAPAS FRITAS',
+        precio: 900,
+        imagen: './imagen/hambur.png'
+    },
+    {
+        id: 6,
+        nombre: 'VEGUI',
+        descripcion:'Pan artesanal, Doble medallon de soja, Queso Tybo, lechuga, tomate, huevo, PAPAS FRITAS',
+        precio: 800,
+        imagen: './imagen/hambur.png'
+    },
+    
 ]
 
 const cardPoductos = () => {
@@ -184,14 +52,67 @@ const cardPoductos = () => {
         card.innerHTML = `<img src="${producto.imagen}" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">${producto.nombre}</h5>
-          <p class="card-text">${producto.precio}</p>
-          <a href="#" class="btn btn-primary" onClick="agregarAlcarrito()">Agregar al Carrito</a>
+          <p class="card-text">${producto.descripcion}</p>
+          <p class="card-text">${producto.precio}$</p>
+          <a href="#" class="btn btn-primary" onClick="agregarAlcarrito(${indice})">Agregar al Carrito</a>
         </div>`
         contenedor.appendChild(card)
     })
 }
 cardPoductos()
 
-const agregarAlcarrito = () => {
-    alert('producto agregado al carrito')
+let carrito = []
+let divCarrito = document.getElementById("carrito")
+
+const agregarAlcarrito = (indice) => {
+    const indiceCarrito = carrito.findIndex((producto)=>{
+        producto.id === productos[indice].id 
+    })
+    if (indiceCarrito === -1) {
+        const productoAgregar = productos[indice]
+        productoAgregar.cantidad = 1
+        carrito.push(productoAgregar)
+        dibujarCarrito()
+    } else {
+        carrito[indiceCarrito].cantidad +=1
+        dibujarCarrito()
+    }
+}
+
+let total = 0
+
+const dibujarCarrito = ()=>{
+    divCarrito.className = "carrito"
+    divCarrito.innerHTML = ""
+    if(carrito.length > 0){
+        carrito.forEach((producto, indice) => {
+            total = total + producto.precio * producto.cantidad
+            const carritoContainer = document.createElement("div")
+            carritoContainer.className = "producto-carrito"
+            carritoContainer.innerHTML = `
+            <img class="car-img" src="${producto.imagen}">
+            <div class="producto-details">${producto.nombre}</div>
+            <div class="producto-details">Catindad: ${producto.cantidad}</div>
+            <div class="producto-details">Precio: ${producto.precio}</div>
+            <div class="producto-details">Sub Tolal: ${producto.precio * producto.cantidad}</div>
+            <button class="btn btn-primary" id="remove-product" onclick="removeProduct(${indice})">Eliminar</button>`
+
+            divCarrito.appendChild(carritoContainer)
+        });
+        
+        const totalContainer = document.createElement("div")
+        totalContainer.className = "total-carrito"
+        totalContainer.innerHTML = `<div class= "total"> TOTAL $ ${total}</div>
+        <button class= "btn btn-primary finalizar" id= "finalizar" onClick= "finalizarCompra()"> FINALIZAR </button>
+        `
+        divCarrito.appendChild(totalContainer)
+
+    } else {
+        divCarrito.classList.remove("carrito")
+    }
+}
+
+const removeProduct = ()=>{
+    carrito.splice(indice, 1)
+    dibujarCarrito()
 }
